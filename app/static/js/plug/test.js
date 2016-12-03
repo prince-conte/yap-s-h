290dbe51d5fef515f5ba0eeb6da082289fba01ba
prince-conte <prince-conte@outlook.com>
2016-12-03 18:42:58 +0300

    init(1/2++h--) // yap(1.2)

--- app/static/js/plug/test.js
+++ app/static/js/plug/test.js
@@ -1,7 +1,71 @@
  $(document).ready(function() {
      
      
-   // alert('Yap!') 
+$('.comments__one').slick({
+  centerMode: true,
+  centerPadding: '370px',
+  slidesToShow: 1,
+  autoplay: true,
+  arrows: false,
+  dots: false,
+  autoplaySpeed: 4000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+     
+     
+       
+     
+$('.comments__two').slick({
+  centerMode: true,
+  centerPadding: '50px',
+  slidesToShow: 2,
+  autoplay: true,
+  arrows: false,
+  dots: false,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+     
+     
+     
      
      
 });
